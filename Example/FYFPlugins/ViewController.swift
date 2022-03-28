50269c2bfa6e40867ac3f1ce911bbdd6bc463989
范云飞 <fyf786452470@gmail.com>
2022-03-28 15:18:55 +0800

    更新demo

--- Example/FYFPlugins/ViewController.swift
+++ Example/FYFPlugins/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import FYFWebController
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+    var webViewController: FYFWebViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let htmlButton = UIButton()
+        htmlButton.setTitle("原生文件", for: .normal)
+        htmlButton.setTitleColor(.black, for: .normal)
+        htmlButton.addTarget(self, action: #selector(htmlClick), for: .touchUpInside)
+        self.view.addSubview(htmlButton)
+        htmlButton.snp.makeConstraints { make in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(self.view).offset(300)
+            make.size.equalTo(CGSize(width: 80, height: 30))
+        }  
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func htmlClick() {
+        
+        let webVC: FYFWebViewController = FYFWebViewController.init(webViewUrl: "https://luna.gtjaqh.com/news-static/html/2021/0825/af7c33e841a0e3bc02cabf66b590e2e5.html")
+        webVC.isUserNativeNavBar = true
+        webVC.showShareItem = true
+        self.navigationController?.pushViewController(webVC, animated: true)
+    }
 }
-
